602d0a5788c4748f61a6bc3e90264ce5eb2922bc
botanic235121 <ilyxa888@gmail.com>
2019-06-04 19:40:38 +0300

    Removing unnecessary files. README was updated.

--- src/main/java/com/sinitsyn/manager/ParkingLifeCycle.java
+++ src/main/java/com/sinitsyn/manager/ParkingLifeCycle.java
@@ -7,11 +7,13 @@ import com.sinitsyn.manager.entity.Vehicle;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
 public class ParkingLifeCycle {
     private ArrayList<Vehicle> mockVehiclesReadyToParking;
     private volatile ArrayList<Vehicle> vehiclesAlreadyInParking;
     private ParkingManager parkingManager;
+    private static final Logger LOGGER = Logger.getLogger(ParkingLifeCycle.class.getSimpleName());
 
 
     public ParkingLifeCycle() {
@@ -33,24 +35,25 @@ public class ParkingLifeCycle {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         executorService.execute(() -> {
-            for (int i = 0; i < 200; i++) {
+            for (int i = 0; i < 20; i++) {
                 try {
-                    Thread.sleep(600);
+                    Thread.sleep(900);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    LOGGER.warning(String.format("Unexpected exception. Details: \n %s",e.getMessage()));
                 }
                 Vehicle vehicle = mockVehiclesReadyToParking.get((int) (Math.random() * mockVehiclesReadyToParking.size()));
+                mockVehiclesReadyToParking.remove(vehicle);
                 parkingManager.putVehicleIntoParking(vehicle);
                 vehiclesAlreadyInParking.add(vehicle);
             }
         });
 
         executorService.execute(() -> {
-            for (int i = 0; i < 200; i++) {
+            for (int i = 0; i < 20; i++) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    LOGGER.warning(String.format("Unexpected exception. Details: \n %s",e.getMessage()));
                 }
                 if (!vehiclesAlreadyInParking.isEmpty()) {
                     Vehicle vehicle = vehiclesAlreadyInParking.get(0);
